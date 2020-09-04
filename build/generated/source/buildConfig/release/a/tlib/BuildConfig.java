cc1b88ff5eb7a85b7940520f457fefa6071a6452
tianguisen <375156836@qq.com>
2020-09-04 13:56:54 +0800

    chore(gitignore):清除git缓存，解决gitignore问题

---
+++ build/generated/source/buildConfig/release/a/tlib/BuildConfig.java
@@ -0,0 +1,22 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package a.tlib;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "a.tlib";
+  /**
+   * @deprecated APPLICATION_ID is misleading in libraries. For the library package name use LIBRARY_PACKAGE_NAME
+   */
+  @Deprecated
+  public static final String APPLICATION_ID = "a.tlib";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 102;
+  public static final String VERSION_NAME = "1.5.7";
+  // Fields from build type: release
+  public static final boolean IS_DEBUG = false;
+  public static final boolean IS_RELEASE = true;
+  public static final String LOGIN_CODE = "";
+}
--- src/main/java/a/tlib/utils/retrofit/ResWrapper.kt
+++
@@ -1,8 +0,0 @@
-package com.lb.baselib.retrofit
-
-
-data class ResWrapper<out T>(val code: Int = -1, val message: String? = null, val data: T?) {
-    override fun toString(): String {
-        return "ResWrapper(code=$code, message=$message, data=$data)"
-    }
-}

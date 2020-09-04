71812ccd70231ebe1ad55f4397249d181a7f900d
tianguisen <375156836@qq.com>
2020-09-04 11:40:25 +0800

    1

---
+++ build/generated/source/buildConfig/debug/a/tlib/BuildConfig.java
@@ -0,0 +1,22 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package a.tlib;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "a.tlib";
+  /**
+   * @deprecated APPLICATION_ID is misleading in libraries. For the library package name use LIBRARY_PACKAGE_NAME
+   */
+  @Deprecated
+  public static final String APPLICATION_ID = "a.tlib";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 102;
+  public static final String VERSION_NAME = "1.5.7";
+  // Fields from build type: debug
+  public static final boolean IS_DEBUG = true;
+  public static final boolean IS_RELEASE = false;
+  public static final String LOGIN_CODE = "85966157";
+}

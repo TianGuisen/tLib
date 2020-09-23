b0041036cf9127e9952982d060758b394f65fcec
tianguisen <375156836@qq.com>
2020-09-23 16:35:28 +0800

    ResCode.kt

--- src/main/java/a/tlib/utils/retrofit/ResCode.kt
+++ src/main/java/a/tlib/utils/retrofit/ResCode.kt
@@ -5,7 +5,7 @@ object ResCode {
     /**
      * 重新登录
      */
-    const val TOKEN_OVERDUE = 12
+    const val TOKEN_OVERDUE = 4040
     //失败
     const val RESPONSE_ERROR = -1
     const val RESPONSE_ERROR2 = 1003

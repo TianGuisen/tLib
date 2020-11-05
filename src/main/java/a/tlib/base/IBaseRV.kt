0f8f7d3c06070bbccd732a122db3ac0a61647cdb
tianguisen <375156836@qq.com>
2020-11-05 17:32:29 +0800

    短视频

--- src/main/java/a/tlib/base/IBaseRV.kt
+++ src/main/java/a/tlib/base/IBaseRV.kt
@@ -20,6 +20,7 @@ interface IBaseRV<T : IRVListBean, B : BaseQuickAdapter<T, BaseViewHolder>> {
     var lv: LoadView?
     var enableloadMore: Boolean
     fun initRVView() {
+        rv.scheduleLayoutAnimation()
         rv.adapter = adapter
         srl?.setOnRefreshListener {
             page = 1

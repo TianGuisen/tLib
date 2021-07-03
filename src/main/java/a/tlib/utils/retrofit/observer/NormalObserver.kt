3ca47a7111768198efa25c9639a985d071e00de5
tianguisen <375156836@qq.com>
2021-07-03 10:56:41 +0800

    lib修改

--- src/main/java/a/tlib/utils/retrofit/observer/NormalObserver.kt
+++ src/main/java/a/tlib/utils/retrofit/observer/NormalObserver.kt
@@ -2,30 +2,35 @@ package com.lb.baselib.retrofit
 
 import Interceptors
 import a.tlib.logger.YLog
-import a.tlib.utils.ToastUtil
 import a.tlib.utils.retrofit.ApiErrorType
 import a.tlib.utils.retrofit.LoadView
 import a.tlib.utils.retrofit.ResWrapper
-import android.content.Context
-import android.os.Looper
 import a.tlib.logger.YLog2
 import com.scwang.smartrefresh.layout.SmartRefreshLayout
 import io.reactivex.SingleObserver
-import io.reactivex.disposables.Disposable
 import retrofit2.HttpException
 import java.net.ConnectException
 import java.net.SocketTimeoutException
 import java.net.UnknownHostException
+import a.tlib.utils.ProgressDiaUtil
+import a.tlib.utils.ToastUtil
+import a.tlib.utils.retrofit.*
+import android.content.Context
+import android.os.Looper
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import com.jeremyliao.liveeventbus.LiveEventBus
+import io.reactivex.disposables.Disposable
 
 /**
  * 网络请回调处理
  */
-abstract class NormalObserver<T> : SingleObserver<ResWrapper<T>>, IObserver<T> {
-    override var lv: LoadView? = null
-    override var context: Context? = null
-    override var tag: String? = null
-    override var showToast: Boolean = true
-    override var jumpLogin: Boolean = false
+abstract class NormalObserver<T> : SingleObserver<ResWrapper<T>> {
+    var lv: LoadView? = null
+    var context: Context? = null
+    var tag: String? = null
+    var showToast: Boolean = true
+    var jumpLogin: Boolean = false
     var srl: SmartRefreshLayout? = null
 
     /**
@@ -109,5 +114,62 @@ abstract class NormalObserver<T> : SingleObserver<ResWrapper<T>>, IObserver<T> {
             onFinish()
         }
 
+    }
+
+    abstract fun onSucces(t: ResWrapper<T>)
+
+    open fun onFailure(t: ResWrapper<T>?) {
+
+    }
+
+    /**
+     * 不管成功或失败都会调用
+     */
+    open fun onFinish() {
+
+    }
+
+    fun startHandle(d: Disposable) {
+        context?.let {
+            if (it is FragmentActivity) {
+                ProgressDiaUtil.show(it)
+            } else if (context is Fragment) {
+                ProgressDiaUtil.show(context as Fragment)
+            } else {
+                ProgressDiaUtil.show(it)
+            }
+        }
+    }
+
+    fun finishHandle() {
+        context?.let {
+            ProgressDiaUtil.dismiss()
+        }
+    }
+
+    fun showErrorToast(t: ResWrapper<T>) {
+        if (showToast) {
+            if (Looper.myLooper() == Looper.getMainLooper()) {
+                ToastUtil.normal(t.message)
+            }
+        }
+    }
+
+    fun isSuccess(t: ResWrapper<T>): Boolean {
+        return t.code == ResCode.RESPONSE_SUCCESS2 || t.code == ResCode.RESPONSE_SUCCESS || t.code == ResCode.RESPONSE_SUCCESS3
+    }
+
+    /**
+     * 检查登录
+     */
+    fun checkLogin(t: ResWrapper<T>): Boolean {
+        if (t.code == ResCode.TOKEN_OVERDUE || t.code == ResCode.TOKEN_OVERDUE2) {
+            if (jumpLogin) {
+                //跳转到登陆界面
+                LiveEventBus.get(ResCode.TOKEN_OVERDUE.toString()).post(null)
+            }
+            return false
+        }
+        return true
     }
 }

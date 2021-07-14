b07942fab23750444a61dc9caefa169d5529f3ed
tianguisen <375156836@qq.com>
2021-07-14 15:30:59 +0800

    ResWrapper.kt增加时间戳

--- src/main/java/a/tlib/utils/retrofit/ResWrapper.kt
+++ src/main/java/a/tlib/utils/retrofit/ResWrapper.kt
@@ -1,7 +1,10 @@
 package a.tlib.utils.retrofit
 
 
-data class ResWrapper<out T>(val code: Int = -1, val message: String? = null, val data: T?) {
+data class ResWrapper<out T>(val code: Int = -1,
+                             val message: String? = null,
+                             val data: T?,
+                             val timestamp: Long? = 0) {
     override fun toString(): String {
         return "ResWrapper(code=$code, message=$message, data=$data)"
     }

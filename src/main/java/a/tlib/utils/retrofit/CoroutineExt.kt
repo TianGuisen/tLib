f8b99b68abd4018db0cce5e91cc1690bdaf6206a
TianGUisen <375156836@qq.com>
2022-01-20 17:36:20 +0800

    1

--- src/main/java/a/tlib/utils/retrofit/CoroutineExt.kt
+++ src/main/java/a/tlib/utils/retrofit/CoroutineExt.kt
@@ -1,14 +1,22 @@
 package a.tlib.utils.retrofit
 
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
+import okio.IOException
+import retrofit2.Retrofit
+import java.net.ConnectException
 import kotlin.contracts.InvocationKind
 import kotlin.coroutines.ContinuationInterceptor
 import kotlin.coroutines.CoroutineContext
 
 /**
  * @author 田桂森 2022/1/20 0020
+ * 协程扩展
+ */
+/**
+ * 切换到主线程里执行
  */
 suspend fun <T> withMain(block: suspend CoroutineScope.() -> T) = withContext(Dispatchers.Main, block)
+/**
+ * 切换到io里执行
+ */
 suspend fun <T> withIO(block: suspend CoroutineScope.() -> T) = withContext(Dispatchers.IO, block)
